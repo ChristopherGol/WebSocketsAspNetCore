7370e854c61e726364968fa4f048f605933aec44
krzysztof.goljasz@gmail.com <krzysztof.goljasz@gmail.com>
2017-05-23 11:53:23 +0200

    Add WebSocket middleware and the page tests mechanism.

--- src/WebSockets/Startup.cs
+++ src/WebSockets/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Net.WebSockets;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
 
 namespace WebSockets
 {
@@ -46,6 +49,41 @@ namespace WebSockets
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            
+            // Use WebSockets middleware. 
+            app.UseWebSockets(new WebSocketOptions() {
+
+                  // Frequency sends "ping" to the client. 
+                  KeepAliveInterval=TimeSpan.FromSeconds(120),
+
+                  // The size of buffer used to receive data. 
+                  ReceiveBufferSize=4*1024
+            });
+
+            // Accept WebSocket requests. 
+            app.Use(async (context, next) =>
+            {
+                // Check if WebSocket requests come in '/ws' path. 
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        // The method 'AcceptWebSocketAsync' switch to TCP connection and return WebSocket object (send and receive messages). 
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+                        // The method 'Echo' receive message and send the same message to the client. 
+                        await Echo(context, webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+            });
 
             app.UseStaticFiles();
 
@@ -53,8 +91,25 @@ namespace WebSockets
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=WebSocket}/{action=Index}/{id?}");
             });
         }
+
+        // The method receive message and send the same message to the client. 
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+
     }
 }
---
+++ src/WebSockets/wwwroot/js/websocket.js
@@ -0,0 +1,87 @@
+﻿$(document).ready(function () {
+
+    // WebSocket object. 
+    var socket;
+
+    // Protocol for WebSocket requests. 
+    var scheme = document.location.protocol == "https:" ? "wss" : "ws";
+
+    // Get port. 
+    var port = document.location.port ? (":" + document.location.port) : "";
+
+    // Set server URL for connection to WebSocket.
+    $("#serverURL").val(scheme + "://" + document.location.hostname + port + "/ws");
+
+    // The function is responsible for open connection to WebSocket.
+    $("#connectServer").on('click', function (e) {
+
+        // Initialize WebSocket object with Url server. 
+        socket = new WebSocket($("#serverURL").val());
+        stateWebSocket();
+
+        socket.onopen = function (event) {
+            $('#logs').append('<tr><td colspan="3">Connection opened.</td></tr>');
+        };
+
+        socket.onclose = function (e) {
+            $('#logs').append('<tr><td colspan="3">Connection closed. Code: '+e.code+'. Reason: ' + e.reason +'</td></tr>');
+        };
+
+        socket.onmessage = function (e) {
+            $('#logs').append('<tr><td>Server</td><td>Client</td><td>' + e.data + '</td></tr>');
+        };
+
+    });
+
+    // The function is responsible for close connection to WebSocket.
+    $("#closeSocket").on('click', function (e) {
+
+        // Close WebSocket connection. 
+        socket.close(1000, "Closed from client.");
+
+        stateWebSocket();
+
+    });
+
+    // The function is responsible for send message to server using WebSocket. 
+    $("#sendMessage").on('click', function (e) {
+
+        var data = $("#message").val();
+
+        // Send data from client to Server using WebSocket. 
+        socket.send(data);
+
+        $('#logs').append('<tr><td>Client</td><td>Server</td><td>' + data + '</td></tr>');
+        stateWebSocket();
+
+    });
+
+    // The function is responsible for display WebSocket object states. 
+    function stateWebSocket() {
+
+        switch (socket.readyState) {
+
+            case WebSocket.OPEN:
+                $('#statusWebSocket').text('WebSocket opened');
+                break;
+
+            case WebSocket.CLOSED:
+                $('#statusWebSocket').text('WebSocket closed.');
+                break;
+
+            case WebSocket.CONNECTING:
+                $('#statusWebSocket').text('WebSocket connecting...');
+                break;
+
+            case WebSocket.CLOSING:
+                $('#statusWebSocket').text('WebSocket closing...');
+                break;
+
+            default:
+                $('#statusWebSocket').text('Unknown state: ' + socket.readyState);
+                break;
+
+        }
+    }
+
+});
